101f1d64750096303d535da8b0b64230f511f60b
geiver12 <sosdani2000@hotmail.com>
2020-05-13 19:52:54 -0400

    -version 1.0

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+ScrollReveal().reveal('.showcase', { delay: 300 })
+
+ScrollReveal().reveal('.news-cards ', { delay: 200 })
+
+ScrollReveal().reveal('.cards-banner-one', { delay: 300 })
+
+ScrollReveal().reveal('.cards-banner-two', { delay: 300 })
